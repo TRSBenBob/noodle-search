b21b331e1a7795b2146318234273b7d92a429ee6
Ben Rymill <ben.rymill@gmail.com>
2025-02-11 15:37:18 +0000

    Add tailwind and daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  eslint: { config: { standalone: false } },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
